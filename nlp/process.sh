7077bca27edbaca8788a084d9bdacbc5a48a5c69
Santina Lin <santina424@gmail.com>
2020-11-13 11:44:36 -0800

    Script on how to get and process abstract

---
+++ nlp/process.sh
@@ -0,0 +1,12 @@
+
+# TODO: download the data 
+
+# get the abstract
+wget https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-11-12/metadata.csv
+pip install csvtool
+cvstool -c 9 metadata.csv > abstracts.txt
+
+# get the packages
+pip install scispacy
+pip install spacy
+pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.3.0/en_ner_bc5cdr_md-0.3.0.tar.gz
